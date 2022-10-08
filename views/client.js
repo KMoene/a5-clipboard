29e21379b2d38bd104db2968abfdcc4193ee623a
Oliver Chen <hchen7@wpi.edu>
2022-10-08 19:06:58 -0400

    basic functionality

---
+++ views/client.js
@@ -0,0 +1,38 @@
+// called when posting message
+const handleNewMessage = function (e) {
+    // prevent default form action from being carried out
+    e.preventDefault()
+
+    const message = document.querySelector('#newMessage').value,
+        password = document.querySelector('#password').value,
+        // encrypt the message
+        ciphertext = CryptoJS.AES.encrypt(message, password).toString(),
+        json = { message: ciphertext, action: "new" },
+        body = JSON.stringify(json)
+
+    fetch('/submit', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body
+    })
+    .then(async function (response) {
+        let data = await response.json()
+        // Decrypt debug
+        var bytes  = CryptoJS.AES.decrypt(data.msg, password);
+        var originalText = bytes.toString(CryptoJS.enc.Utf8);
+        console.log(data.mid);
+        console.log(data.msg);
+        console.log(originalText);
+
+        // adding info
+        const origin = window.location.host
+        document.getElementById("msgLink").textContent =  origin+"/s/?m=" + data.mid
+    })
+}
+
+
+window.onload = function () {
+    const addMessage_btn = document.querySelector('#addNewMessage')
+    addMessage_btn.onclick = handleNewMessage
+}
+
